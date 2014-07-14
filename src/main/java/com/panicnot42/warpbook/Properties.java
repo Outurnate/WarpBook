aa603c4e4266bb8fbeda3c0d68f65efda9bbc42b
josephd <joe@w00t42.net>
2014-07-14 18:04:08 -0400

    Version bump

--- src/main/java/com/panicnot42/warpbook/Properties.java
+++ src/main/java/com/panicnot42/warpbook/Properties.java
@@ -3,6 +3,6 @@ package com.panicnot42.warpbook;
 public class Properties
 {
   public static final String modid = "warpbook";
-  public static final String version = "1.1.40";
+  public static final String version = "1.1.42";
   public static final String name = "Warp Book";
 }
