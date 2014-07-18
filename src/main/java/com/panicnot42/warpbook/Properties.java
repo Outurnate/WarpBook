c18e2bf8449ded5ddfc21d629c4d37f0d10fa9d5
josephd <joe@w00t42.net>
2014-07-18 17:59:43 -0400

    Added build system subs

--- src/main/java/com/panicnot42/warpbook/Properties.java
+++ src/main/java/com/panicnot42/warpbook/Properties.java
@@ -3,6 +3,6 @@ package com.panicnot42.warpbook;
 public class Properties
 {
   public static final String modid = "warpbook";
-  public static final String version = "1.1.42";
+  public static final String version = "${version}";
   public static final String name = "Warp Book";
 }
